be2e9fba18052976618e7d6bb808a80c9ae40a57
Jody <koenig6@byui.edu>
2019-09-24 10:58:13 -0400

    setting up php home page and fixed core assignment for team assignment

--- web/js/firstBtn.js
+++ web/js/firstBtn.js
@@ -1,4 +1,4 @@
-function clicked() {
-    alert('Clicked!');
-}
-
+var div = document.getElementById("first");
+div.children[1].onclick = function(){
+   div.style.backgroundColor = div.children[0].value;
+};
--- web/js/secondBtn.js
+++ web/js/secondBtn.js
@@ -1,4 +1,4 @@
-var div = document.getElementsByClassName("first");
-div.children[1].onclick = function(){
-   div.style.backgroundColor = div.children[0].value;
-};
+function clicked() {
+    alert('Clicked!');
+}
+
