833cfa48a871b7c70dd4b11b2d90651cf8c1c757
Jody <koenig6@byui.edu>
2019-09-24 16:58:38 -0400

    starting css for main page

---
+++ web/assignments/homePage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Jody Koenig CS 313</title>
+        <link href="css/" rel="stylesheet">
+
+    </head>
+    <body>
+        <h1>Jody Koenig - CS 313 - Home Page</h1>
+
+        <nav>
+            <ul class="navigation">
+            <li>  </li>
+            <li>  </li>
+            </ul>
+
+        </nav>
+
+    <?php
+
+        echo "<h1>test</h1>";
+    ?>
+
+        <script src="js/"></script>
+    </body>
+</html>
