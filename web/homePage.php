117201e680b8c70d7a979cab2963cd191814cee2
Jody <koenig6@byui.edu>
2019-09-25 10:35:14 -0400

    add normalize and fonts

--- web/homePage.php
+++ web/homePage.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
          <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Jody Koenig CS 313</title>
+        <!-- external style references in the proper cascading order -->
+        <link href="https://fonts.googleapis.com/css?family=Oswald|Slabo+27px" rel="stylesheet">
+        <link href="css/normalize.css" rel="stylesheet"> <!-- normalize useragent/browser defaults -->
         <link href="css/main.css" rel="stylesheet">
 
     </head>
