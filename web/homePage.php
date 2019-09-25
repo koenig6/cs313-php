a8d479c5286677daf9633f782bfa7923e9ee3927
Jody <koenig6@byui.edu>
2019-09-25 11:48:05 -0400

    now adding photos

--- web/homePage.php
+++ web/homePage.php
@@ -30,6 +30,25 @@
 
         <main>
 
+            <div class="index">
+                <div>
+                <img class="home" src="images/eric-nopanen-3skLpaOBlMw-unsplash.jpg" alt="bicyclists riding in sidewalk in open field" width="400" height="266">
+                <h3>Family Biking</h3>
+                <p>.  </p>
+                </div>
+
+                <div>
+                <img class="home" src="images/markus-spiske-AfKyYsE9j6w-unsplash.jpg" alt="two bicycles in the sunset" width="400" height="269">
+                <h3>Thinking of getting a mountain bike?</h3>
+                <p>.</p>
+                </div>
+            </div>
+
+                <img class="home" src="images/matt-bowden-GZc4fnQsaWQ-unsplash.jpg" alt="bicycle racers" width="400" height="266">
+            <h3>Ride the longest off-pavement route in the world.</h3>
+            <p>.</p>
+
+
             <p>
             I am pretty much someone who loves to be home with my family.  I love going to Six Flags and river rafting and plan to do as much as I can of both!
             </p>
