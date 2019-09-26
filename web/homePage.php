a085612c6dc4cbae187d6f2697520ac7fa89e289
Jody <koenig6@byui.edu>
2019-09-26 11:16:49 -0400

    added more details about me

--- web/homePage.php
+++ web/homePage.php
@@ -39,20 +39,20 @@
                 <div>
                     <img class="home"  onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/eric-nopanen-3skLpaOBlMw-unsplash.jpg" alt="people by the pool" width="200" height="133">
                     <h3>Time with family and friends</h3>
-                    <p>I love spending time with my friends and family doing what ever.  </p>
+                    <p>I am a mother of four and I love spending time with my friends and family doing anything.  </p>
                 </div>
 
                 <div>
                     <img class="home" onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/markus-spiske-AfKyYsE9j6w-unsplash.jpg" alt="lap top on desk" width="200" height="133">
                     <h3>School</h3>
-                    <p> I am about half way through my school career </p>
+                    <p> I have always wanted to get my college education.  Now that my kids are in school I am taking advantage of the opportunity online education offers.  I am majoring in Software Engineering</p>
                 </div>
 
 
                 <div>
                     <img class="home" onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/matt-bowden-GZc4fnQsaWQ-unsplash.jpg" alt="roller coaster" width="200" height="133">
                     <h3>Riding roller coasters</h3>
-                    <p> As often as my husband and I can find free time we take our dates to six flags.</p>
+                    <p> As often as my husband and I can find free time we take our dates to six flags. I love all the rides, whether they are the easy going ones or the biggest and fastest.</p>
                 </div>
             </div>
 
