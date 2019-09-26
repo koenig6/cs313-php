f539219954c03b3d4abe7ff9bd2f4d9917b441cb
Jody <koenig6@byui.edu>
2019-09-26 11:09:22 -0400

    adjusted divs

--- web/homePage.php
+++ web/homePage.php
@@ -37,23 +37,24 @@
 
             <div class="index">
                 <div>
-                <img class="home"  onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/eric-nopanen-3skLpaOBlMw-unsplash.jpg" alt="people by the pool" width="200" height="133">
-                <h3>Time with family and friends</h3>
-                <p>I love spending time with my friends and family doing what ever.  </p>
+                    <img class="home"  onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/eric-nopanen-3skLpaOBlMw-unsplash.jpg" alt="people by the pool" width="200" height="133">
+                    <h3>Time with family and friends</h3>
+                    <p>I love spending time with my friends and family doing what ever.  </p>
                 </div>
 
                 <div>
-                <img class="home" onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/markus-spiske-AfKyYsE9j6w-unsplash.jpg" alt="lap top on desk" width="200" height="133">
-                <h3>School</h3>
-                <p> I am about half way through my school career </p>
+                    <img class="home" onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/markus-spiske-AfKyYsE9j6w-unsplash.jpg" alt="lap top on desk" width="200" height="133">
+                    <h3>School</h3>
+                    <p> I am about half way through my school career </p>
+                </div>
+
+
+                <div>
+                    <img class="home" onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/matt-bowden-GZc4fnQsaWQ-unsplash.jpg" alt="roller coaster" width="200" height="133">
+                    <h3>Riding roller coasters</h3>
+                    <p> As often as my husband and I can find free time we take our dates to six flags.</p>
                 </div>
             </div>
-
-                <img class="home" onmouseover="bigNav(this)" onmouseout="normalNav(this)" src="images/matt-bowden-GZc4fnQsaWQ-unsplash.jpg" alt="roller coaster" width="200" height="133">
-            <h3>Riding roller coasters</h3>
-            <p> As often as my husband and I can find free time we take our dates to six flags.</p>
-
-
 
         </main>
 
