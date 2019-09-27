5c18820aaf8190b89aa40591ae8ae500d5484bf0
Jody <koenig6@byui.edu>
2019-09-26 21:26:21 -0400

    attempting jquery

--- web/js/firstBtn.js
+++ web/js/firstBtn.js
@@ -1,4 +1,16 @@
-var div = document.getElementById("first");
+/*stackoverflow */
+
+/*var div = document.getElementById("first");
 div.children[1].onclick = function(){
    div.style.backgroundColor = div.children[0].value;
-};
+};*/
+
+
+        $(function(){
+
+            $('#firstBtn').click(function(){
+                ('submit').css("color","text")
+            });
+
+        });
+
