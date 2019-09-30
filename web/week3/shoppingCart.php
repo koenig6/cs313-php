f22cb02cda2acbef33166e9438e37d380f0fb7e4
Jody <koenig6@byui.edu>
2019-09-30 11:14:48 -0400

    addng new file shopping cart

---
+++ web/week3/shoppingCart.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/main.css">
+        <title>Shopping Cart</title>
+
+    </head>
+    <body>
+        <nav>
+            <ul class="navigation">
+                <li class="active">Browse Items</li>
+
+                <li>>View Cart</li>
+                <li>>Checkout</li>
+                <li>>Confirmation</li>
+            </ul>
+        </nav>
+
+
+    </body>
+
+    <script src="js/firstBtn.js"></script>
+
+
+</html>
