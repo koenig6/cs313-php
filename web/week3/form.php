1f3b1420f065fb5c8a96c7c79cf9b62483ee463d
Jody <koenig6@byui.edu>
2019-10-01 10:54:12 -0400

    tea

--- web/week3/form.php
+++ web/week3/form.php
@@ -4,9 +4,10 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Your email address is: <?php echo $_POST["email"]; ?><br>
 Your major is: <?php echo $_POST["major"]; ?><br>
 Your comments: <?php echo $_POST["comment"]; ?> <br>
+Your continents: <?php echo $_POST["continents"]; ?> <br>
 
 </body>
 </html>
