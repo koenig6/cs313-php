17f52abb54dd9e1600e776aa07b8678d849e4d7c
Jody <koenig6@byui.edu>
2019-10-02 15:15:34 -0400

    adding space

--- web/week3/confirmation.php
+++ web/week3/confirmation.php
@@ -27,7 +27,7 @@ session_start();
         </nav>
 
             Thank your for your purchase <?php echo $_POST["firstName"]; ?><br>
-            Your delivery will be to: <?php echo $_POST["firstName"];echo $_POST["lastName"]; ?><br>
+            Your delivery will be to: <?php echo $_POST["firstName"]; echo " "; echo $_POST["lastName"]; ?><br>
                                         <?php echo $_POST["email"]; ?><br>
                                         <?php echo $_POST["street"]; echo $_POST["state"];echo $_POST["zip"];?><br>
 
