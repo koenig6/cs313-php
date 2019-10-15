4e0cae82d76f6e084765aeece7507bfb730f879c
Jody <koenig6@byui.edu>
2019-10-15 12:11:29 -0400

    trying to connect to db

--- web/week4/movie.php
+++ web/week4/movie.php
@@ -29,6 +29,27 @@
 
             <TITLE>Your Personal Movie Database</TITLE>
 
+            <?php
+            try
+            {
+                $dbUrl = getenv('DATABASE_URL');
+                $dbOpts = parse_url($dbUrl);
+                $dbHost = $dbOpts["host"];
+                $dbPort = $dbOpts["port"];
+                $dbUser = $dbOpts["user"];
+                $dbPassword = $dbOpts["pass"];
+                $dbName = ltrim($dbOpts["path"],'/');
+
+                $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $ex)
+            {
+                echo 'Error!: ' . $ex->getMessage();
+                die();
+            }
+            ?>
+
 
 
             <form action="movie.php" method="post">
