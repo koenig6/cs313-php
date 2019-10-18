26c93da659212e575df5582b3718f3eb75cf59e4
Jody <koenig6@byui.edu>
2019-10-18 14:02:41 -0400

    test

--- web/week4/movieResults.php
+++ web/week4/movieResults.php
@@ -54,7 +54,26 @@ session_start();
 
 
 
-            $query = 'SELECT * FROM movie';
+            $query = 'SELECT
+	m.title as movie_title,
+	m.year as movie_year,
+	m.description as movie_desc,
+	a.actorsfirstname as fname,
+	a.actorslastname as lname,
+	g.genrename as genre,
+	r.rating as rating,
+	s.studioname as studio,
+
+	*
+FROM
+	movie as m
+	left join studio as s on m.studioid = s.studioid
+	left join genre as g on m.genreid = g.genreid
+	left join rating as r on m.ratingid = r.ratingid
+	left join movietoactor as ma on m.movieid = ma.movieid
+	left join actors as a on ma.actorsid = a.actorsid
+WHERE
+    1=1';
 
                     foreach ($db->query($query)as $row) {
                         echo $row['title'] . ', <br>';
