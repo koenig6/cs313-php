ed9fc9f631f82fabf6cd2bc726a3bded0f6ba1dd
Jody <koenig6@byui.edu>
2019-10-19 10:17:40 -0400

    test

--- web/week4/movieResults.php
+++ web/week4/movieResults.php
@@ -143,9 +143,9 @@ WHERE
             <?php
 
                 foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $row){
-                    echo ' <div class="grid-item">'$row['movie_title'] . ' - ' . $row['movie_year'] . '
+                    echo $row['movie_title'] . ' - ' . $row['movie_year'] . '
                 <a href="movieSelection.php?title=' . urlencode($row['movie_title']) . '">Select</a><br>
-             ' . $row['fname'] . ' ' . $row['lname'] . ', ' . $row['rating'] . ', ' . $row['genre'] . ', ' . $row['studio'] . '<br>' .$row['movie_desc'] . '<br><br></div>';
+             ' . $row['fname'] . ' ' . $row['lname'] . ', ' . $row['rating'] . ', ' . $row['genre'] . ', ' . $row['studio'] . '<br>' .$row['movie_desc'] . '<br><br>';
 
              ?>
             </div>
