44a4107f366552c8e1eee3eb54f10bbfbe561415
Jody <koenig6@byui.edu>
2019-10-24 14:32:43 -0400

    test

--- web/js/editMovie.js
+++ web/js/editMovie.js
@@ -2,8 +2,8 @@ function addItem(){
     var ul = document.getElementById("myList");
     var newItem = document.getElementsByName("lname"); //get text from box
     var li = document.createElement("li"); // create li
-    li.setAttribute('id',lname.value); //connect li and word from text box
-    li.appendChild(document.createTextNode(item.value)); // add to list
+    li.setAttribute('name',lname.value); //connect li and word from text box
+    li.appendChild(document.createTextNode(lname.value)); // add to list
     ul.appendChild(li); // add to list
 }
 
