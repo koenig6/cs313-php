10461eafdf97f8050f157ae5506d8db16d034078
Jody <koenig6@byui.edu>
2019-10-24 15:57:38 -0400

    test

--- web/js/editMovie.js
+++ web/js/editMovie.js
@@ -12,8 +12,10 @@ function removeItem(){
     var ul = document.getElementById("myList");
     var fname = document.getElementById("fname");
     var lname = document.getElementById("lname");
-    var li = document.getElementById(lname.value + ", " + fname.value);
+    var li = document.getElementById(lname.value);
+    var li2 = document.getElementById(fname.value);
     ul.removeChild(li);
+    ul.removeChild(li2);
 }
 
 
