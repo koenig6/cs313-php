cc9fe530aba17f5abb6aafd23b3ff5691e8c1b9e
Jody <koenig6@byui.edu>
2019-10-26 11:30:10 -0400

    fix

--- web/js/editMovie.js
+++ web/js/editMovie.js
@@ -3,11 +3,11 @@ function addItem(){
     var newItem = document.getElementById("lname"); //get text from box
     var newItem2 = document.getElementById("fname"); //get text from box
     var li = document.createElement("li"); // create li
-    li.setAttribute('id',lname.value); //gives the li an id
-    li.setAttribute('type','hidden');
-    li.setAttribute('name','actors[]');
-    li.setAttribute('value',lname.value + ", " + fname.value); //gives li an id of the item
-    li.appendChild(document.createTextNode(lname.value + ", " + fname.value)); // add to list
+   // li.setAttribute('id',lname.value); //gives the li an id
+   // li.setAttribute('type','hidden');
+   // li.setAttribute('name','actors[]');
+   // li.setAttribute('value',lname.value + ", " + fname.value); //gives li an id of the item
+    li.appendChild(document.createTextNode(lname.value + ", " + fname.value + "<input type=hidden name=actors value=" + lname.value + ", " + fname.value )); // add to list
     ul.appendChild(li); // add to list
 }
 
