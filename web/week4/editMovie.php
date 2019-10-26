13cbe56032e66b5f5715a96229f272a75175b4f9
Jody <koenig6@byui.edu>
2019-10-26 18:21:45 -0400

    clean up

--- web/week4/editMovie.php
+++ web/week4/editMovie.php
@@ -60,7 +60,7 @@
                             $LastName = rtrim(ltrim(strtok($actor, ",")));
                             $FirstName = rtrim(ltrim(strtok(",")));
 
-                            echo "<br>" . $FirstName . " + " . $LastName . "<br>";
+                            echo "<br TOP OF THE PAGE 2>" . $FirstName . " + " . $LastName . "<br>";
 
                             if($FirstName === "" || $LastName === "")
                             {
