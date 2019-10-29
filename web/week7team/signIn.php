b94d331c7b6c945e96797e80572bf0e5e7a63c89
Jody <koenig6@byui.edu>
2019-10-28 21:21:02 -0400

    adding signin page

--- web/week7team/signIn.php
+++ web/week7team/signIn.php
@@ -17,8 +17,9 @@ session_start();
     <header>
 
                <h2>Login Page</h2>
-        </header>
+    </header>
 
+	<body>
         <nav>
             <ul class="navigation">
 
@@ -26,15 +27,39 @@ session_start();
 
             </ul>
         </nav>
-	<body>
 
-		<div>
-                <form action="https://morning-bastion-33855.herokuapp.com/week7team/welcome.php" method="post">
-                    Please enter your username:<input type="text" name="username"><br><br>
-                    password:<input type="password" name="pwd">><br><br>
-                </form>>
-        </div>
-                if(password_verify(string $password, string $hash))
+        <?php
+        //require 'password.php';
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSubmit']))
+        {
+                 try
+                    {
+                            $passwordHash = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
+
+                            //connecting to database
+                            $dbUrl = getenv('DATABASE_URL');
+                            $dbOpts = parse_url($dbUrl);
+                            $dbHost = $dbOpts["host"];
+                            $dbPort = $dbOpts["port"];
+                            $dbUser = $dbOpts["user"];
+                            $dbPassword = $dbOpts["pass"];
+                            $dbName = ltrim($dbOpts["path"],'/');
+
+                            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+                            //getting user info from database for comparision
+                            $queryUser = 'SELECT userpassword FROM users WHERE username = :username';
+                            $stmt = $db->prepare($queryUser);
+                            $stmt->bindValue(':name', $_POST["username"], PDO::PARAM_STR);
+                            $stmt->bindValue(':hashpassword', $passwordHash, PDO::PARAM_STR);
+                            $stmt->execute();
+
+                            header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signin.php");
+
+                            if(password_verify(string $password, string $hash))
                              {
                                  echo 'Password is valid!';
                              }
@@ -42,6 +67,36 @@ session_start();
                              {
                                  echo 'Invalid password';
                              }
+
+
+
+                    }//end try
+                    catch (PDOException $ex)
+                    {
+                        echo 'Error!: ' . $ex->getMessage();
+                        die();
+                    }
+                    catch (Exception $ex)
+                    {
+                        echo 'Error!: ' . $ex->getMessage();
+                        die();
+                    }
+        }//end if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSubmit']))
+        else
+        {
+         ?>
+
+        <div>
+                <form action="" method="post">
+                    Please enter your username:<input type="text" name="username"><br><br>
+                    password (7 letters and a number):<input type="password" name="pwd"><br><br>
+                    password:<input type="password" name="pwd2"><br><br>
+                    <input type="submit" value="addUser" name="btnSubmit" ><br><br>
+
+                </form>
+        </div>
+        <?php }//end else ?>
+
 
 	</body>
 </html>
