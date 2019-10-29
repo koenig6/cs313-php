b923a047252bae73b610e66a33681be3627c93ee
Jody <koenig6@byui.edu>
2019-10-28 20:45:27 -0400

    tst

--- web/week7team/signup.php
+++ web/week7team/signup.php
@@ -28,7 +28,7 @@
 
                  try
                     {
-                            $passwordHash = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
+                            //$passwordHash = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
 
                             //connecting to database
                             $dbUrl = getenv('DATABASE_URL');
@@ -50,7 +50,7 @@
                             $stmt->bindValue(':hashpassword', ($passwordHash, PDO::PARAM_STR));
                             $stmt->execute();
 
-                            header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signin.php");
+                            //header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signin.php");
 
 
 
