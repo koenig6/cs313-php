1183dc3577edeef528468b6d1f7ae667fca74dcb
Jody <koenig6@byui.edu>
2019-10-29 10:56:48 -0400

    d

--- web/week7team/signup.php
+++ web/week7team/signup.php
@@ -26,8 +26,8 @@
         {
             echo "we are here! line 27";
 
-            $pwd1 = $_POST('pwd1');
-            $pwd2 = $_POST('pwd2');
+            //$pwd1 = $_POST('pwd1');
+            //$pwd2 = $_POST('pwd2');
            // if($pwd1 == $pwd2)
            // {
                 echo "we are here! line 33";
