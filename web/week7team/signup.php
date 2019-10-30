24da80a36507a693ec18b636fe57166ad544fd38
Jody <koenig6@byui.edu>
2019-10-29 20:54:33 -0400

    fix

--- web/week7team/signup.php
+++ web/week7team/signup.php
@@ -1,7 +1,7 @@
 <?php
 // Start the session
 session_start();
-$_SESSION["BADPASSWORD"] = "";
+
 ?>
 
 <!DOCTYPE html>
@@ -29,12 +29,22 @@ $_SESSION["BADPASSWORD"] = "";
 
         if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSubmit']))
         {
+            $pwd1 = $_POST['pwd1'];
+            $pwd2 = $_POST['pwd2'];
+
+            if ($pwd1 != $pwd2)
+            {
+                $passError = "Your passwords do not match";
+                $star = "*";
+            }
+
+            if ($pwd1 == $pwd2)
+            {
+
 
                  try
                     {
-                            if ((isset($_POST['pwd1'])) && (isset($_POST['pwd2'])))
-                            {
-                                if(($_POST['pwd1']) == ($_POST['pwd2']))
+
                                 {
 
                                     $passwordHash = password_hash($_POST["pwd1"], PASSWORD_DEFAULT);
@@ -62,15 +72,6 @@ $_SESSION["BADPASSWORD"] = "";
                                     header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signIn.php");
 
 
-                                }//end comparison
-                                else
-                                {
-                                    $_SESSION["BADPASSWORD"] = 1;
-                                    header("Location: https://morning-bastion-33855.herokuapp.com/week7team/signup.php");
-
-                                }//end else
-                            }//end isset
-
 
                     }//end try
                     catch (PDOException $ex)
@@ -84,31 +85,22 @@ $_SESSION["BADPASSWORD"] = "";
                         die();
                     }
 
+                }// end if ($pwd1 == $pwd2)
 
         }//end if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnSubmit']))
        else
         {
-           echo $_"BADPASSWORD"];
-           if($_SESSION["BADPASSWORD"] == 1)
-           {
-              echo "<h3 style='color:red;'>Passwords Do Not Match</h3>";
-           }
-             echo "here is session var " . $_SESSION["BADPASSWORD"];
-             echo "line 97";
+
          ?>
 
         <div>
-
-                <form onsubmit="return isIdentical()" method="post">
-                    Please enter your username:<input type="text" name="username"><br><br>
-                    password (7 letters and a number):<input type="password" id ="pwd1" name="pwd1" onkeyup="checkPassword()"><br><br>
-                    password:<input type="password"  id="pwd2" name="pwd2" onkeyup="checkPassword()"><br><br>
-                     <span id="error" style="color:red"></span>
-		            <span id="error2" style="color:red"></span>
-                    <input type="submit" value="addUser" name="btnSubmit"  ><br><br>
-
-                </form>
-        </div>
+            <form action="" method="post">
+              Please enter your username:<input type="text" name="username"><br><br>
+              password (7 letters and a number):<input type="password" id ="pwd1" name="pwd1"><span style="color:red;"><?php echo $star; ?></span><br><br>
+              password:<input type="password"  id="pwd2" name="pwd2"><span style="color:red;"><?php echo $star; ?></span><br><br>
+              <input type="submit" value="addUser" name="btnSubmit"  ><span style="color:red;"><?php echo $passError; ?></span><br><br>
+                 </form>
+  </div>
       <?php }//end else ?>
 
 
