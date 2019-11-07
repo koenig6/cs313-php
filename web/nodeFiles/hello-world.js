23187d417495a91f9461aab7c28ff2a56bac9b06
Jody <koenig6@byui.edu>
2019-11-07 10:57:52 -0500

    adding home file and server

--- web/nodeFiles/hello-world.js
+++ web/nodeFiles/hello-world.js
@@ -1,12 +1,1 @@
-var http= require('http');
-
-function sayHello(request, responce)
-{
-    console.log('Recieved a request for : ' + request.url);
-
-}
-
-var server = http.createServer(sayHello);
-server.listen(5000);
-
-console.log('The server is now listening on port 5000....');
+console.log('Hello World!');
---
+++ web/nodeFiles/localNodeProve.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function onRequest (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+      //200 means everything is ok
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
