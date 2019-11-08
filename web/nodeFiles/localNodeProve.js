09c9ffe237b631d0381feeff973a1f49aa475ee4
Jody <koenig6@byui.edu>
2019-11-08 16:45:22 -0500

    a

--- web/nodeFiles/localNodeProve.js
+++ web/nodeFiles/localNodeProve.js
@@ -1,18 +1,31 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var http = require('http'); // Import Node.js core module
 
-http.createServer(function onRequest (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+var server = http.createServer(function onRequest(req, res) {   //create web server
+    if (req.url == '/home') { //check the URL of the current request
+
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        // set response content
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><h1>Welcome to the Home Page!</h1></html>');
+        res.end();
+
     }
-      //200 means everything is ok
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8888);
+    else if (req.url == "/getData")
+    {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({"name":"Jody","class":"CS 313"}));
+        res.end();
+
+    }
+    else
+        res.writeHead(400, { 'Content-Type': 'text/html' });
+        res.write('404 - Page Not Found');
+        res.end();
+});
+
+server.listen(5000); //6 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
